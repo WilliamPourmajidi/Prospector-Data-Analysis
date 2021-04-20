653fc651f9efeeeb1ab28bc266061ec020cead9c
William Pourmajidi <William.Pourmajidi@gmail.com>
2021-04-20 09:10:44 -0400

    initial Commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+prospector_data = pd.read_json('Prospector_report.json')
+
+# print(prospector_data)
+print(prospector_data.index)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
